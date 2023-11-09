4ea7ea75ddbc3a9c0825a63f9436adda75086187
profalexresende <82842833+profalexresende@users.noreply.github.com>
2023-11-09 19:59:25 -0300

    limpeza

--- js/fbconfig.js
+++ js/fbconfig.js
@@ -1,20 +1,18 @@
 const firebaseConfig = {
-
-    apiKey: "SUA_API_KEY",
-    authDomain: "SEU_DOMINIO.firebaseapp.com",
-    projectId: "SEU_ID_DE_PROJETO",
-    storageBucket: "SEU_STORAGE_BUCKET",
-    messagingSenderId: "SEU_SENDER_ID",
-    appId: "SEU_APP_ID"
-    
-    };
-  
+    apiKey: "AIzaSyBu5YbmrNkLR6VAEoaZbBNlSp5cqyX04r0",
+    authDomain: "etec23-e0755.firebaseapp.com",
+    databaseURL: "https://etec23-e0755-default-rtdb.firebaseio.com",
+    projectId: "etec23-e0755",
+    storageBucket: "etec23-e0755.appspot.com",
+    messagingSenderId: "77824979520",
+    appId: "1:77824979520:web:90c7ba8013ef053de2a5a5"
+};  
     // Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig);
   
     // Firebase Database Reference and the child
-const dbRef = firebase.database().ref();
-const pessoaRef = dbRef.child('avaliacaoip');
+//const dbRef = firebase.database().ref();
+//const pessoaRef = dbRef.child('avaliacaoip');
 
 var storage = firebase.storage();
   
